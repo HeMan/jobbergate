985cbcdebacc56d24303316615bf498b66ce6ecf
Jimmy Hedman <jimmy.hedman@gmail.com>
2019-10-16 16:18:02 +0200

    Remove debug info.

--- appform.py
+++ appform.py
@@ -3,9 +3,7 @@ questions = []
 
 def Text(variablename, question, default=None):
     questions.append({"variablename": variablename, "question": question})
-    print(variablename, question, default)
 
 
 def Integer(variablename, question, minval=None, maxval=None, default=None):
     questions.append({"variablename": variablename, "question": question})
-    print(variablename, question, minval, maxval, default)
