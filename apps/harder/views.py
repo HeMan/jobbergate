7936e6024b212c7b33a796df02a2f34fe516d8cd
Jimmy Hedman <jimmy.hedman@gmail.com>
2019-10-17 08:53:19 +0200

    Move appform.py.

--- apps/harder/views.py
+++ apps/harder/views.py
@@ -1,4 +1,4 @@
-import appform
+from jobbergate import appform
 
 appform.Text("jobname", "Name of job", default="my-job")
 appform.Integer("memory", "Max memory needed (GB)", minval=0, maxval=1000, default=10)
--- apps/simple/views.py
+++ apps/simple/views.py
@@ -1,6 +1,4 @@
-import appform
+from jobbergate import appform
 
-ask_jobname = appform.Text("jobname", "Name of job", default="my-job")
-ask_memory = appform.Integer(
-    "memory", "Max memory needed (GB)", minval=0, maxval=1000, default=10
-)
+appform.Text("jobname", "Name of job", default="my-job")
+appform.Integer("memory", "Max memory needed (GB)", minval=0, maxval=1000, default=10)
