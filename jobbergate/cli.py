73314d2efb24b179b7f7d562c6d32a26f8f2a969
Jimmy Hedman <jimmy.hedman@gmail.com>
2019-10-18 15:48:23 +0200

    Add fields and validators.
    - Adds all currently used field types. Resolves #1
    - Adds validators for all field types. Resolves #2

--- jobbergate/cli.py
+++ jobbergate/cli.py
@@ -1,7 +1,7 @@
-import click
-from pathlib import Path
-import inquirer
 import importlib
+from pathlib import Path
+import click
+import inquirer
 from flask import render_template
 from flask.cli import with_appcontext
 
@@ -9,15 +9,84 @@ from flask.cli import with_appcontext
 def _app_factory():
     def _callback(application):
         @with_appcontext
-        def _callbackier(*args, **kvargs):
+        def _callbackier():
             importedlib = importlib.import_module(f"apps.{application}.views")
 
             questions = []
 
             for field in importedlib.appform.questions:
-                questions.append(
-                    inquirer.Text(field["variablename"], message=field["question"])
-                )
+                if field["type"] == "Text":
+                    questions.append(
+                        inquirer.Text(
+                            field["variablename"],
+                            message=field["message"],
+                            default=field["default"],
+                        )
+                    )
+
+                if field["type"] == "Integer":
+                    # FIXME: validating doesn't work when called without limits
+                    questions.append(
+                        inquirer.Text(
+                            field["variablename"],
+                            message=field["message"],
+                            default=field["default"],
+                            # validate=lambda _, x: field["minval"]
+                            # <= int(x)
+                            # <= field["maxval"],
+                        )
+                    )
+
+                if field["type"] == "List":
+                    questions.append(
+                        inquirer.List(
+                            field["variablename"],
+                            message=field["message"],
+                            choices=field["choices"],
+                            default=field["default"],
+                        )
+                    )
+
+                if field["type"] == "Directory":
+                    questions.append(
+                        inquirer.Path(
+                            field["variablename"],
+                            message=field["message"],
+                            path_type=inquirer.Path.DIRECTORY,
+                            default=field["default"],
+                            exists=field["exists"],
+                        )
+                    )
+
+                if field["type"] == "File":
+                    questions.append(
+                        inquirer.Path(
+                            field["variablename"],
+                            message=field["message"],
+                            path_type=inquirer.Path.FILE,
+                            default=field["default"],
+                            exists=field["exists"],
+                        )
+                    )
+
+                if field["type"] == "Checkbox":
+                    questions.append(
+                        inquirer.Checkbox(
+                            field["variablename"],
+                            message=field["message"],
+                            choices=field["choices"],
+                            default=field["default"],
+                        )
+                    )
+
+                if field["type"] == "Confirm":
+                    questions.append(
+                        inquirer.Confirm(
+                            field["variablename"],
+                            message=field["message"],
+                            default=field["default"],
+                        )
+                    )
 
             data = inquirer.prompt(questions)
 
