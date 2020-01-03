e4ce5f38ac57c510e2cfdaec76bb90f0b73710de
Jimmy Hedman <jimmy.hedman@gmail.com>
2020-01-03 15:12:37 +0100

    Finally got reimport to work!

--- jobbergate/lib.py
+++ jobbergate/lib.py
@@ -8,8 +8,11 @@ with open("jobbergate.yaml") as ymlfile:
 
 def fullpath_import(path, lib):
     """Imports a file from absolute path."""
+    if lib in sys.modules:
+        del sys.modules[lib]
     sys.path.append(f"{config['apps']['path']}/{path}/")
-    module = importlib.import_module(lib)
-    sys.path.remove(f"{config['apps']['path']}/{path}/")
-
+    try:
+        module = importlib.import_module(lib)
+    finally:
+        sys.path.remove(f"{config['apps']['path']}/{path}/")
     return module
