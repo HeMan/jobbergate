33e9766832bcf89c51d3bcdf075831346dc2dffb
Wang Zhengyu <zhengyu.wang@scania.com>
2020-03-06 12:54:09 +0100

    Remove redundant parentes

--- jobbergate/cli.py
+++ jobbergate/cli.py
@@ -366,7 +366,7 @@ def app_factory():
             jinjatemplate = jinjaenv.get_template(template)
             file = outputfile.write(jinjatemplate.render(data=data))
             outputfile.close()
-            if (data.keys().__contains__("cmd_command")):
+            if data.keys().__contains__("cmd_command"):
                 subprocess.run(data['cmd_command'].split(), stdout=True, check=True)
             return file
 
