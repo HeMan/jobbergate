93fbc23ad034749f82f69851a3bf43f2372dfa03
Wang Zhengyu <zhengyu.wang@scania.com>
2020-03-18 16:58:15 +0100

    Add jobbergateconfig = {} in jobbergatepath check block

--- jobbergate/lib.py
+++ jobbergate/lib.py
@@ -15,6 +15,7 @@ jobbergatepath = os.getenv("JOBBERGATE_PATH", None)
 
 if jobbergatepath is None:
     print("Undefined JOBBERGATE_PATH")
+    jobbergateconfig = {}
     sys.exit(0)
 
 if os.path.isabs(jobbergatepath):
