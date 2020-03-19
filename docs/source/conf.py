51a716c11294934bd5f421d989a400d02759d132
Nils S <nils.soderback@scania.com>
2020-03-19 16:38:41 +0100

    Bump version: 1.4.0 → 1.4.1-dev0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Jimmy Hedman"
 author = "Jimmy Hedman"
 
 # The full version, including alpha/beta/rc tags
-release = "1.4.0"
+release = "1.4.1-dev0"
 
 
 # -- General configuration ---------------------------------------------------
